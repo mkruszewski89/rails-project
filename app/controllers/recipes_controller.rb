2c918badcabbbe9a6c8a0a4b84565ed4b1bf0d23
github username <github email address>
2018-03-20 17:20:55 -0400

    Recipe controller new, create, edit, update, destroy skeleton actions; Recipe views _form, new, edit skeletons

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,7 +5,23 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+  end
+
   def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 
   private
