9883442c5ce9cf516830acd74e59316effbfcdbb
github username <github email address>
2018-03-20 16:31:56 -0400

    recipe index controller action and view with links to recipe show view and user show view; recipe #user_name

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,9 @@
-# class Recipe < ApplicationRecord
-#   belongs_to :user
-#   has_many :recipe_ingredients
-#   has_many :ingredients, through: :recipe_ingredients
-#
-#   def user_name
-#     user.name
-#   end
-# end
+class Recipe < ApplicationRecord
+  belongs_to :user
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+
+  def user_name
+    user.name
+  end
+end
