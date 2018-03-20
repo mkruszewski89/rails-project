a6b8494d3c4877e9afc5622490f196fc22b39553
github username <github email address>
2018-03-20 16:27:19 -0400

    add name column to users table

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,9 @@
-class Recipe < ApplicationRecord
-  belongs_to :user
-  has_many :recipe_ingredients
-  has_many :ingredients, through: :recipe_ingredients
-end
+# class Recipe < ApplicationRecord
+#   belongs_to :user
+#   has_many :recipe_ingredients
+#   has_many :ingredients, through: :recipe_ingredients
+#
+#   def user_name
+#     user.name
+#   end
+# end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320193925) do
+ActiveRecord::Schema.define(version: 20180320202515) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20180320193925) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", default: "fill in your name!"
   end
 
 end
