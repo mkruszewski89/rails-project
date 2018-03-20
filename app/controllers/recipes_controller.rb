58b0d1fa0a5d923828e90920f8aca88494ab0cd6
github username <github email address>
2018-03-20 16:34:20 -0400

    before_action #set_recipe on RecipesController show, edit, update, destroy

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,14 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+
   def index
     @recipes = Recipe.all
   end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
 end
