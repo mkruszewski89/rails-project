d897aa4aca352322e17c356c7461e6759585192e
github username <github email address>
2018-03-22 15:46:01 -0400

    Recipe model #convert_ingredient_quantity_to(ingredient_name, unit_name

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -16,10 +16,20 @@ class Recipe < ApplicationRecord
     }
   end
 
+  def convert_ingredient_quantity_to(ingredient_name, unit_name)
+    pieces = ingredient_quantity(ingredient_name).split(" ")
+    unit_new = Unit.find_by(name: unit_name)
+    unit_old = Unit.find_by(name: pieces[1].singularize)
+    quantity_old = pieces[0].to_f
+    quantity_lowest_unit = quantity_old * unit_old.lowest_unit_equivalence
+    quantity_new = quantity_lowest_unit / unit_new.lowest_unit_equivalence
+    "#{quantity_new.round(2)} #{unit_new.name.pluralize(quantity_new)}"
+  end
+
   def ingredient_quantity(ingredient_name)
     ingredient_object = ingredients.detect {|ingredient| ingredient.name = ingredient_name}
     quantity_object = quantities.detect {|quantity| quantity.ingredient = ingredient_object}
-    "#{quantity_object.amount} #{quantity_object.unit.name.pluralize(quantity_object.amount)}"
+    "#{quantity_object.amount.round(2)} #{quantity_object.unit.name.pluralize(quantity_object.amount)}"
   end
 
 end
