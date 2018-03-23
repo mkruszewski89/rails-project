15f966777f6c09fb5e76eee99298e825d151f5f8
github username <github email address>
2018-03-23 10:07:56 -0400

    refactor Recipe views _form, edit, new to use partial with locals; Quantity model #unit_name and #ingredient_name

--- app/models/quantity.rb
+++ app/models/quantity.rb
@@ -2,4 +2,12 @@ class Quantity < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
   belongs_to :unit
+
+  def unit_name
+    unit.name.pluralize(amount)
+  end
+
+  def ingredient_name
+    ingredient.name
+  end
 end
