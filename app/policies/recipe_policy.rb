fbfac1bf66cf21ed98e8b1d5293ec5c0b6fab8c2
github username <github email address>
2018-03-26 16:31:03 -0400

    add user logged in to Recipe policies ro avoid 'undefined method for nil class' error when visiting pages that conditionally allow update and destroy

--- app/policies/recipe_policy.rb
+++ app/policies/recipe_policy.rb
@@ -1,15 +1,15 @@
 class RecipePolicy < ApplicationPolicy
 
   def edit?
-    user.try && (user.admin? || record[:user] == user)
+    user.try(:email) && (user.admin? || record.try(:user) == user)
   end
 
   def update?
-    user.try && (user.admin? || record[:user] == user)
+    user.try(:email) && (user.admin? || record.try(:user) == user)
   end
 
   def destroy?
-    user.try && (user.admin? || record[:user] == user)
+    user.try(:email) && (user.admin? || record.try(:user) == user)
   end
 
 end
